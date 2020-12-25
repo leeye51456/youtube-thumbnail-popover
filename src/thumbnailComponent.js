dd0bcec9d60c38e0a2e22b7414507405d39ec1fb
leeye51456 <leeye51456@gmail.com>
2020-12-26 04:51:26 +0900

    Add partial thumbnails

--- src/thumbnailComponent.js
+++ src/thumbnailComponent.js
@@ -4,17 +4,29 @@ let component = null;
 const thumbnails = {
   videoId: '',
   title: null,
+  beginning: null,
+  middle: null,
+  end: null,
 };
+
+const createImgElement = function createImgElementWithAttributes({ width, height, className }) {
+  const img = document.createElement('img');
+  img.width = width;
+  img.height = height;
+  img.className = className;
+  return img;
+}
 
 const initializeThumbnail = function initializeThumbnailComponent() {
   const container = document.createElement('div');
   container.className = 'ytpext-thumbnail-container';
 
-  thumbnails.title = document.createElement('img');
-  thumbnails.title.width = 320;
-  thumbnails.title.height = 180;
-  thumbnails.title.className = 'ytpext-thumbnail-title';
+  thumbnails.title = createImgElement({ width: 320, height: 180, className: 'ytpext-thumbnail-title'});
   container.appendChild(thumbnails.title);
+  for (const name of ['beginning', 'middle', 'end']) {
+    thumbnails[name] = createImgElement({ width: 320, height: 180, className: 'ytpext-thumbnail-part'});
+    container.appendChild(thumbnails[name]);
+  }
 
   component = container;
 }
@@ -25,6 +37,9 @@ const updateThumbnail = function updateThumbnailComponent(videoId) {
   }
 
   thumbnails.title.src = `https://i.ytimg.com/vi_webp/${videoId}/mqdefault.webp`;
+  thumbnails.beginning.src = `https://i.ytimg.com/vi_webp/${videoId}/mq1.webp`;
+  thumbnails.middle.src = `https://i.ytimg.com/vi_webp/${videoId}/mq2.webp`;
+  thumbnails.end.src = `https://i.ytimg.com/vi_webp/${videoId}/mq3.webp`;
   thumbnails.videoId = videoId;
   return component;
 };
