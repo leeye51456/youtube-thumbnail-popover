d7ecfc31a73b3395aacb8dc7326a84e4eb4abb27
leeye51456 <leeye51456@gmail.com>
2020-12-30 18:19:11 +0900

    Implement strict mode of `getVideoId`

--- src/urlUtils.js
+++ src/urlUtils.js
@@ -1,3 +1,9 @@
+const shortUrlPathnameId = /^\/([0-9A-Za-z_-]{11})/;
+const shortUrlPathnameIdPrefix = /^\/([0-9A-Za-z_-]{1,11})/;
+
+const fullUrlIdQuery = /^([0-9A-Za-z_-]{11})/;
+const fullUrlIdQueryPrefix = /^([0-9A-Za-z_-]{1,11})/;
+
 const getUrlWithHttps = function supposeProtocolOfUrlHttps(urlString) {
   if (!urlString.startsWith('https://')) {
     return urlString.replace(/^(.+:\/\/)?/, 'https://');
@@ -5,16 +11,17 @@ const getUrlWithHttps = function supposeProtocolOfUrlHttps(urlString) {
   return urlString;
 };
 
-export const getVideoId = function getYouTubeVideoIdFromUrl(urlString) {
+export const getVideoId = function getYouTubeVideoIdFromUrl(urlString, strict) {
   const url = new URL(getUrlWithHttps(urlString.startsWith('/') ? location.host + urlString : urlString));
   const { hostname, pathname, searchParams } = url;
 
+  let matches;
   if (hostname === 'youtu.be') {
-    const matches = /^\/([0-9A-Za-z_-]+)/.exec(pathname);
-    return matches ? matches[1] : null;
+    matches = (strict ? shortUrlPathnameId : shortUrlPathnameIdPrefix).exec(pathname);
   } else if (/^((www|m)\.)?youtube\.com$/.test(hostname) && pathname.startsWith('/watch') && searchParams.has('v')) {
-    return searchParams.get('v').replace(/[^0-9A-Za-z_-]+/, '');
+    matches = (strict ? fullUrlIdQuery : fullUrlIdQueryPrefix).exec(searchParams.get('v'));
   } else {
     return null;
   }
+  return matches ? matches[1] : null;
 };
